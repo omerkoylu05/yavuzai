53fdbcd90983cdb423744fdcfc277b84f485ddf2
omerkoylu05 <omer.koylu.05@gmail.com>
2024-09-15 17:30:02 +0300

    Paz 15 Eyl 2024 17:30:01 +03

--- src/yavuz/yavuz_v2.js
+++ src/yavuz/yavuz_v2.js
@@ -73,7 +73,7 @@ let ready=false;
     //     // };
     //   };
     Module.onRuntimeInitialized = function() {
-        Module._set_depth(2);
+        Module._set_depth(3);
         //console.log("module loaded...");
     }
     //   //console.log(Module);
